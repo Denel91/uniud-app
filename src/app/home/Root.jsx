353693bc5102adebf4fb710cd07c4e54e8ed106c
Daniele Di Salvo <disi91@hotmail.it>
2023-12-20 15:32:17 +0100

    Update Home layout and visuals in Root.jsx

--- src/app/home/Root.jsx
+++ src/app/home/Root.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Image} from "@nextui-org/react";
 import {Layout} from "../../components/Layout.jsx";
 
 const Polygon = () => (
@@ -65,14 +64,14 @@ const Scenario = () => (
 export const Home = () => {
     return (
         <Layout>
-            <div className="bg-white">
+            <div className="bg-white relative overflow-hidden isolate">
                 <div className="relative isolate px-6 pt-20 lg:px-8">
                     <Polygon/>
-                    <div className="mx-auto max-w-7xl px-6 py-4 sm:py-28 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-24">
-                        <div className="mx-auto max-w-2xl lg:mx-0 lg:flex-auto">
-                            <Headline
-                                className="mt-10 py-3 max-w-xl text-6xl font-bold tracking-tight text-gray-900 sm:text-7xl">
-                                Take <span className="text-red-600">control</span> of your health
+                    <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
+                        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl lg:flex-shrink-0 lg:pt-8">
+                            <img className="h-11" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"/>
+                            <Headline className="mt-10 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                                Take <span className="text-indigo-600">control</span> of your health
                             </Headline>
                             <Paragraph className="mt-6 text-lg leading-8 text-gray-600">
                                 Esse id magna consectetur fugiat non dolor in ad laboris magna laborum ea consequat.
@@ -83,12 +82,16 @@ export const Home = () => {
                                 </LinkBlock>
                             </div>
                         </div>
-                        <div className="mt-12 sm:mt-16 lg:mt-0 lg:flex-shrink-0 lg:flex-grow">
-                            <Image alt="" width={720} src="/pulmonologist.svg"/>
+                        <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
+                            <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
+                                <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4 lg:ml-2">
+                                    <img src="/Adobe.jpeg" alt="App screenshot" width={800} height={1280} className="w-[76rem] rounded-md shadow-2xl ring-1 ring-gray-900/10"/>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                </div>
             </div>
-        </div>
             <Scenario/>
         </Layout>
     );

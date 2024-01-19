fbb898008c4314648610f1c6e31ad691fdb80d3f
Daniele Di Salvo <disi91@hotmail.it>
2024-01-19 16:24:10 +0100

    Updated the Header component to utilize 'Link' instead of 'a' HTML element and renamed the title of the main HTML page.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,10 +34,10 @@ export const Header = () => {
         <header className="absolute inset-x-0 top-0 z-50">
             <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div className="flex lg:flex-1">
-                    <a href="#" className="-m-1.5 p-1.5">
-                        <span className="sr-only">Your Company</span>
-                        <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""/>
-                    </a>
+                    <Link to="/" className="-m-1.5 p-1.5">
+                        <span className="sr-only">Uniud</span>
+                        <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="logo_header"/>
+                    </Link>
                 </div>
                 <div className="flex lg:hidden">
                     <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" onClick={() => setMobileMenuOpen(true)}>

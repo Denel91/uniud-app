9ee1c4ae6beffb60b34236f6ddcc020b773dd28d
Daniele Di Salvo <disi91@hotmail.it>
2024-01-25 15:13:58 +0100

    Optimized image loading on Groups and Home pages
    Introduced the "loading=lazy" attribute to images on both the Groups and Home pages to improve page load speed. .

--- src/app/groups/Groups.jsx
+++ src/app/groups/Groups.jsx
@@ -58,7 +58,7 @@ export const Groups = () => {
                     {
                         posts.map((post) => (
                             <article key={post.id} className="relative isolate flex flex-col justify-center overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
-                                <img rel="preload" src={post.imageUrl} alt={post.description} className="absolute inset-0 -z-10 h-full w-full object-cover"/>
+                                <img rel="preload" loading="lazy" src={post.imageUrl} alt={post.description} className="absolute inset-0 -z-10 h-full w-full object-cover"/>
                                 <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40"/>
                                 <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10"/>
                                 <h3 className="mt-3 text-lg font-semibold leading-6 text-white">
@@ -74,7 +74,7 @@ export const Groups = () => {
                     {
                         postsUnder.map((post) => (
                             <article key={post.id} className="relative isolate flex flex-col justify-center overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
-                                <img rel="preload" src={post.imageUrl} alt={post.description} className="absolute inset-0 -z-10 h-full w-full object-cover"/>
+                                <img rel="preload" loading="lazy" src={post.imageUrl} alt={post.description} className="absolute inset-0 -z-10 h-full w-full object-cover"/>
                                 <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40"/>
                                 <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10"/>
                                 <h3 className="mt-3 text-lg font-semibold leading-6 text-white">
--- src/app/home/Root.jsx
+++ src/app/home/Root.jsx
@@ -80,7 +80,7 @@ export const Home = () => {
                         <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
                             <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
                                 <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4 lg:ml-2">
-                                    <img rel="preload" loading="lazy" src="/Home-min.jpeg" alt="App screenshot" width={800} height={1280} className="w-[76rem] rounded-md shadow-2xl ring-1 ring-gray-900/10"/>
+                                    <img rel="preload" loading="lazy" src="/home-min.jpeg" alt="App screenshot" width={800} height={1280} className="w-[76rem] rounded-md shadow-2xl ring-1 ring-gray-900/10"/>
                                 </div>
                             </div>
                         </div>

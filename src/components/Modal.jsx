406016cce3275042d52a9afc22b50071d0af61cb
Daniele Di Salvo <disi91@hotmail.it>
2024-02-25 15:45:15 +0100

    Update modal title and button text in Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -50,7 +50,7 @@ export const Modal = ({data, onClose, resetData}) => {
                                     </div>
                                     <div className="mt-4 text-center sm:mt-5">
                                         <Dialog.Title as="h3" className="text-xl font-semibold leading-6 text-gray-900">
-                                            Diagnosis Report
+                                            Group Hypothesis
                                         </Dialog.Title>
                                         <div className="mt-6">
                                             <p className="text-xl text-black">
@@ -60,7 +60,7 @@ export const Modal = ({data, onClose, resetData}) => {
                                     </div>
                                 </div>
                                 <div className="mt-5 sm:mt-6">
-                                    <Button type="button" onClick={handleClose} radius="lg"  size="md" variant="flat" className="inline-flex w-full justify-center bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back to diagnostic tool</Button>
+                                    <Button type="button" onClick={handleClose} radius="lg"  size="md" variant="flat" className="inline-flex w-full justify-center bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back to educational tool</Button>
                                 </div>
                             </Dialog.Panel>
                         </Transition.Child>
